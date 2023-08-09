c81f9a7511c2466315bd2d231eaa2aeceeb87701
slanesuke <slaneian@gmail.com>
2023-08-09 10:30:24 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::process;
+use minigrep::Config;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing argument {}", err);
+        process::exit(1);
+    });
+
+    println!("Search for {}", config.query);
+    println!("In file {}", config.filename);
+
+    // if the call to run() executes into an error variant then execute the block of code
+    // thee block of code runs the prints a status code of 1.
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+
+}
+
+
